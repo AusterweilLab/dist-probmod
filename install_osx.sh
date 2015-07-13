339a03464192154377ab537b69d6f151205d41de
Johnny Snelgrove <johnny.snelgrove@gmail.com>
2015-07-13 19:53:24 -0400

    Update install_osx.sh

--- install_osx.sh
+++ install_osx.sh
@@ -5,9 +5,10 @@ echo "downloading and installing formdiscovery"
 mkdir ./formdiscovery1.0
 curl http://www.psy.cmu.edu/~ckemp/code/formdiscovery1.0.tar.gz | tar xz -C ./formdiscovery1.0
 echo "downloading and installing nauty25r9"
-curl http://cs.anu.edu.au/~bdm/nauty/nauty25r9.tar.gz | tar xz -C ./nauty25r9
+curl http://cs.anu.edu.au/~bdm/nauty/nauty25r9.tar.gz | tar xz
 cd nauty25r9
 ./configure
 make
 cd ..
+rm nauty25r9.tar.gz
 echo "installation finished!"
