10f1d78eec210720a51b6baaa0dd637218ed53cb
jsnelgro <johnny.snelgrove@gmail.com>
2015-07-13 18:05:19 -0400

    fix formdiscovery url

--- install_linux.sh
+++ install_linux.sh
@@ -2,9 +2,12 @@
 # Install script
 echo "beginning installation..."
 echo "downloading and installing formdiscovery"
+mkdir ./formdiscovery1.0
+cd ./formdiscovery1.0/
 wget http://charleskemp.com/code/formdiscovery1.0.tar.gz
 tar xvzf formdiscovery1.0.tar.gz
 rm formdiscovery1.0.tar.gz
+cd ..
 echo "downloading and installing nauty25r9"
 wget http://cs.anu.edu.au/~bdm/nauty/nauty25r9.tar.gz
 tar xvzf nauty25r9.tar.gz
