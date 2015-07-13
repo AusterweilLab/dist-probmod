aeb94fa10a6a80ea3798e46a6758a2eeadbe3296
jsnelgro <johnny.snelgrove@gmail.com>
2015-07-13 17:14:22 -0400

    make diff install scripts for osx and linux

---
+++ install_linux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Install script
+echo "beginning installation..."
+echo "downloading and installing formdiscovery"
+wget http://charleskemp.com/code/formdiscovery1.0.tar.gz
+tar xvzf formdiscovery1.0.tar.gz
+rm formdiscovery1.0.tar.gz
+echo "downloading and installing nauty25r9"
+wget http://cs.anu.edu.au/~bdm/nauty/nauty25r9.tar.gz
+tar xvzf nauty25r9.tar.gz
+rm nauty25r9.tar.gz
+cd nauty25r9
+./configure
+make
+cd ..
+echo "installation finished!"
